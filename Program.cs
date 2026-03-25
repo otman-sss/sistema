aa8e6e156fe8f0f71616c7dcb48092ff7b7ec3e1
Alicia <hdz38697@gmail.com>
2026-03-25 11:52:39 -0700

    proyecto

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using CapaPresentacion1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaPreparatoria2
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new CapaPresentacion1.FrmMenu());
+            FrmLogin login = new FrmLogin();
+
+            if (login.ShowDialog() == DialogResult.OK)
+            {
+                Application.Run(new FrmMenu());
+            }
+        }
+    }
+}
